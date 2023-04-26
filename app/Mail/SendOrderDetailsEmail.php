bf71ac47be88ee1d62d2d23a835d26a8b20f74be
Slobodan <slobodan.novak@gmail.com>
2023-04-27 00:12:42 +0200

    made email send in background

--- app/Mail/SendOrderDetailsEmail.php
+++ app/Mail/SendOrderDetailsEmail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendOrderDetailsEmail extends Mailable
+class SendOrderDetailsEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
